57e8b6b9eea8abcbda15ce49ee52b524f64ee3c3
Yves1812 <yves.bonnefont@gmail.com>
2019-05-08 19:58:14 +0200

    Created a version with OTA NOT working

--- appentis/fuel_monitoring/fuel_monitoring_OTA.ino/fuel_monitoring_OTA.ino.ino
+++ appentis/fuel_monitoring/fuel_monitoring_OTA.ino/fuel_monitoring_OTA.ino.ino
@@ -1,19 +1,22 @@
-
-/* SmartHome application developped by Yves Bonnefont to monitor fuel level using MQTT protocol
-2018-04 V1.
-*/
-
 #include <ESP8266WiFi.h>
+#include <ESP8266mDNS.h>
+#include <WiFiUdp.h>
+#include <ArduinoOTA.h>
+
 #include <PubSubClient.h>
 
+#ifndef STASSID
+#define STASSID "Home_Etage"
+#define STAPSK  "mariepascale"
+//#define STASSID "Penn_Ty_Breizh"
+//#define STAPSK  "marie-pascale"
+
+#endif
+
+const char* ssid = STASSID;
+const char* password = STAPSK;
+
 #define debug true
-
-//#define wifi_ssid "Penn_Ty_Breizh"
-//#define wifi_password "marie-pascale"
-#define wifi_ssid "Home_Etage"
-#define wifi_password "mariepascale"
-
-
 #define MAX_TRIES 50
 
 // Pin which we're using
@@ -36,35 +39,6 @@ void callback(char* topic, byte* payload, unsigned int length);
 //Création des objets
 WiFiClient espClient;
 PubSubClient MQTTclient(mqtt_server, 1883, callback, espClient);
-
-//Connexion au réseau WiFi
-void setup_wifi() {
-  int tries=0;
-  int status_wifi = WL_IDLE_STATUS;
-  delay(10);
-  Serial.println();
-  Serial.print("Connexion a ");
-  Serial.print(wifi_ssid);
-    
-  // attempt to connect to Wifi network:
-  WiFi.begin(wifi_ssid, wifi_password);
-  while (WiFi.status() != WL_CONNECTED  && tries<MAX_TRIES) {
-    delay(500);
-    tries=tries+1;
-    Serial.print(".");
-  }
-
-  Serial.println("");
-  if (tries<MAX_TRIES){
-    Serial.println("Connexion WiFi etablie ");
-    Serial.print("=> Addresse IP : ");
-    Serial.println(WiFi.localIP());
-  }
-  else
-  {
-    Serial.println("Failed to connect to WiFi");
-  }
-}
 
 //Connexion - Reconnexion MQTT
 void MQTTreconnect(){
@@ -123,33 +97,90 @@ void sendMQTT(char *topic, float payload)
 }
 
 
-void setup() {
-  Serial.begin(9600);
 
+void setup() {
+// Connect to wifi and set OTA
+  Serial.begin(9600);
+  Serial.println("Booting");
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  while (WiFi.waitForConnectResult() != WL_CONNECTED) {
+    Serial.println("Connection Failed! Rebooting...");
+    delay(5000);
+    ESP.restart();
+  }
+
+  // Port defaults to 8266
+  // ArduinoOTA.setPort(8266);
+
+  // Hostname defaults to esp8266-[ChipID]
+  // ArduinoOTA.setHostname("myesp8266");
+
+  // No authentication by default
+  // ArduinoOTA.setPassword("admin");
+
+  // Password can be set with it's md5 value as well
+  // MD5(admin) = 21232f297a57a5a743894a0e4a801fc3
+  // ArduinoOTA.setPasswordHash("21232f297a57a5a743894a0e4a801fc3");
+
+  ArduinoOTA.onStart([]() {
+    String type;
+    if (ArduinoOTA.getCommand() == U_FLASH) {
+      type = "sketch";
+    } else { // U_SPIFFS
+      type = "filesystem";
+    }
+
+    // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
+    Serial.println("Start updating " + type);
+  });
+  ArduinoOTA.onEnd([]() {
+    Serial.println("\nEnd");
+  });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  });
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) {
+      Serial.println("Auth Failed");
+    } else if (error == OTA_BEGIN_ERROR) {
+      Serial.println("Begin Failed");
+    } else if (error == OTA_CONNECT_ERROR) {
+      Serial.println("Connect Failed");
+    } else if (error == OTA_RECEIVE_ERROR) {
+      Serial.println("Receive Failed");
+    } else if (error == OTA_END_ERROR) {
+      Serial.println("End Failed");
+    }
+  });
+  ArduinoOTA.begin();
+  Serial.println("Ready");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  // Setting up pins and MQTT
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
-
-  /* start the Ethernet connection in loop to correct for hardware disconnections*/
-  setup_wifi();           //On se connecte au réseau wifi
-  delay(1500);
 
   MQTTclient.setServer(mqtt_server, 1883);    //Configuration de la connexion au serveur MQTT
   MQTTclient.setCallback(callback);  //La fonction de callback qui est executée à chaque réception de message
   MQTTreconnect();
   MQTTclient.loop();
+
 }
 
 void loop() {
-  float temp;
+  ArduinoOTA.handle();  float temp;
   long duration;
   float distance;
 
   if (millis()-last_sent > send_period*1000)
   {
     // reconnect to wifi if needed
-    if (WiFi.status() != WL_CONNECTED) {
-      setup_wifi();
-    }
+//    if (WiFi.status() != WL_CONNECTED) {
+//      setup_wifi();
+//    }
     // Reconnect to MQTT broker and re-subscribe
     if (!MQTTclient.connected()) {
       MQTTreconnect();
@@ -163,7 +194,7 @@ void loop() {
     digitalWrite(trigPin, LOW);
     duration = pulseIn(echoPin, HIGH);
     distance = (duration/2) / 29.1;
-    Serial.println(duration); // debug purpose
+//    Serial.println(duration); // debug purpose
     sendMQTT("Niveau_fuel",distance);
     
     last_sent=millis();
